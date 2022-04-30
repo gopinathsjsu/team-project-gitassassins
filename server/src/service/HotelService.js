76441a560f404b3665b42b3d7dd498584bec48ae
Ratika Bhuwalka <ratika.bhuwalka@sjsu.edu>
2022-04-30 09:29:46 -0700

    RoomService

--- server/src/service/HotelService.js
+++ server/src/service/HotelService.js
@@ -7,40 +7,38 @@ class HotelService {
 
 	create = async (req, res) => {
 		try {
-			console.log(req.body);
-			
-			const newHotel = new Hotel({
-                hotelName : req.body.hotelName,
-				hotelAddress: req.body.hotelAddress
-			});
-			const response = await newHotel.save();
+				console.log(req.body);
+				
+				const newHotel = new Hotel({
+					hotelName : req.body.hotelName,
+					hotelAddress : req.body.hotelAddress
+				});
 
-			res.status(200).send(response);
+				const response = await newHotel.save();
+				res.status(200).send(response);
+
 		} catch (err) {
 			console.error(err);
 		}
 	};
 
 
-	get_location = async (req, res) => {
+	get_location = async ( req, res ) => {
 		
 		try {
 				const hotels = await Hotel.find({}, {hotelAddress:1});
-
-			
 				res.status(200).send(hotels);
+
 			} catch (err) {
 				console.error(err);
-				}
+			}
 		};
-
 
 
 	search = async ( req, res ) => {
 		try {
 
 				console.log(req.body);
-				// const hotel = await Hotel.findById(req.body.hotelId).populate("rooms");
 				const booked = await commonService.getHotelReservations(req.body.hotelId, req.body.startDate, req.body.endDate);
 
 				console.log("booked: ", booked.length);
@@ -48,11 +46,9 @@ class HotelService {
 
 
 		} catch (err) {
-		console.error(err);
-	}
-};
-
-
+			console.error(err);
+		}
+	};
 
 }
 
