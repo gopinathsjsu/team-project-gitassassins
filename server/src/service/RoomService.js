d4b44c9e6daad1b4e346bf0780aa13c524ea6340
jaineil <mandaviajaineil@gmail.com>
2022-05-01 00:09:41 -0700

    feat: api - search rooms

--- server/src/service/RoomService.js
+++ server/src/service/RoomService.js
@@ -1,6 +1,6 @@
 import Room from "../model/Room.js";
 
-class RoomService {
+export class RoomService {
 	create = async (req, res) => {
 		try {
 			console.log(req.body);
@@ -22,6 +22,135 @@ class RoomService {
 	};
 
 	// delete can be done
-}
 
-export default RoomService;
+	searchRoomAvailability = async (req, res) => {
+		try {
+			console.log(req.query);
+			const startDate = new Date(req.query.startDate);
+			const endDate = new Date(req.query.endDate);
+
+			const singleRoom = await Room.find({
+				hotelId: hotelId,
+				type: "SINGLE",
+			});
+
+			const kingRoom = await Room.find({
+				hotelId: hotelId,
+				type: "KING",
+			});
+
+			const queenRoom = await Room.find({
+				hotelId: hotelId,
+				type: "QUEEN",
+			});
+
+			const suiteRoom = await Room.find({
+				hotelId: hotelId,
+				type: "SUITE",
+			});
+
+			const singleRoomsBooked = await Reservation.find({
+				hotelId: hotelId,
+				startDate: {
+					$gte: startDate,
+				},
+				endDate: {
+					$lte: endDate,
+				},
+				roomType: "SINGLE",
+			}).count();
+
+			console.log(
+				"Number of SINGLE rooms booked in that date range: ",
+				singleRoomsBooked
+			);
+
+			const kingRoomsBooked = await Reservation.find({
+				hotelId: hotelId,
+				startDate: {
+					$gte: startDate,
+				},
+				endDate: {
+					$lte: endDate,
+				},
+				roomType: "KING",
+			}).count();
+
+			console.log(
+				"Number of KING rooms booked in that date range: ",
+				kingRoomsBooked
+			);
+
+			const queenRoomsBooked = await Reservation.find({
+				hotelId: hotelId,
+				startDate: {
+					$gte: startDate,
+				},
+				endDate: {
+					$lte: endDate,
+				},
+				roomType: "QUEEN",
+			}).count();
+
+			console.log(
+				"Number of QUEEN rooms booked in that date range: ",
+				queenRoomsBooked
+			);
+
+			const suiteRoomsBooked = await Reservation.find({
+				hotelId: hotelId,
+				startDate: {
+					$gte: startDate,
+				},
+				endDate: {
+					$lte: endDate,
+				},
+				roomType: "SUITE",
+			}).count();
+
+			console.log(
+				"Number of SUITE rooms booked in that date range: ",
+				suiteRoomsBooked
+			);
+
+			const availability = {
+				single: {
+					availableRooms: Math.abs(
+						singleRoom.totalCount - singleRoomsBooked
+					),
+					maximumOccupancy: singleRoom.maximumOccupancy,
+					price: singleRoom.price,
+					photoUrl: singleRoom.photoUrl,
+				},
+				king: {
+					availableRooms: Math.abs(
+						kingRoom.totalCount - kingRoomsBooked
+					),
+					maximumOccupancy: kingRoom.maximumOccupancy,
+					price: kingRoom.price,
+					photoUrl: kingRoom.photoUrl,
+				},
+				queen: {
+					availableRooms: Math.abs(
+						queenRoom.totalCount - queenRoomsBooked
+					),
+					maximumOccupancy: queenRoom.maximumOccupancy,
+					price: queenRoom.price,
+					photoUrl: queenRoom.photoUrl,
+				},
+				suite: {
+					availableRooms: Math.abs(
+						suiteRoom.totalCount - suiteRoomsBooked
+					),
+					maximumOccupancy: suiteRoom.maximumOccupancy,
+					price: suiteRoom.price,
+					photoUrl: suiteRoom.photoUrl,
+				},
+			};
+
+			return res.status(200).send(availability);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+}
